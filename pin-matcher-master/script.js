e470bdf6e311e34e8b8a9da291563ecd47ba987a
nh246 <nazmul210130@diit.edu.bd>
2024-08-16 18:12:48 +0600

    adding code

--- pin-matcher-master/script.js
+++ pin-matcher-master/script.js
@@ -8,11 +8,40 @@ let number
 // the digits 
 
 function clickedNumber(id) {
-    let num = document.getElementById(id).innerText.value;
+    let prevValue = document.getElementById("numPad").value;
+    let num = document.getElementById(id).innerText;
     console.log(num)
+    document.getElementById("numPad").value = prevValue + num ; 
 }
 
+//  clear all 
 
+function clearInput (){
+        document.getElementById("numPad").value = "";
+}
+
+//  remove last digit 
+
+function clearLastDigit(){
+    let numbers = document.getElementById("numPad").value;
+    let newNumbers = numbers.slice(0, numbers.length - 1);
+    document.getElementById("numPad").value = newNumbers;
+}
+
+//  submit 
+
+function submitNums () {
+  let num1 = document.getElementById("numpad").value;
+  let num2 = document.getElementById("displayPin").value;
+
+  let theNewNum1 = parseInt(num1);
+  let theNewNum2 = parseInt(num2);
+
+  if(theNewNum1 == theNewNum2 )
+  {
+    console.log("the pin has matched");
+  }
+}
 
 
 
